ab831797ce8ee45aff49f455fb0e680834e37184
Benjamin Turnbull <benorule@outlook.com>
2020-07-25 16:17:29 +0930

    arranged code in a function so that program can be used for multiple employees

--- server.js
+++ server.js
@@ -2,36 +2,48 @@ var fs = require('fs');
 var readline = require('readline-sync');
 var inquirer = require('inquirer');
 
-// array of questions for user
-const questions = [
-    "Enter your name: ",
-    "Enter your role: ",
-    "Enter your ID numer: ",
-    "Enter your Email address: ",
-    "Enter your GitHub username: "
-];
-
-const answers = [
-];
-
-for (i = 0; i < questions.length; i++) {
-    answers.push(readline.question(questions[i]));
-}
-
-    var stream = fs.createWriteStream("index.html");
+var stream = fs.createWriteStream("index.html");
     stream.once('open', function (fd) {
         stream.write('<!DOCTYPE html> \n <html lang="en"> \n <head> \n <meta charset="UTF-8"> \n <meta name="viewport" content="width=device-width, initial-scale=1.0"> \n <title>Document</title> \n </head> \n <body>');
+
+// const employees = parseInt(readline.question("Enter number of employees: "));
+// console.log(employees);
+
+function getInfo() {
+
+    console.log("test");
+
+    const questions = [
+        "Enter your name: ",
+        "Enter your role: ",
+        "Enter your ID number: ",
+        "Enter your Email address: ",
+        "Enter your GitHub username: "
+    ];
+
+    let answers = [
+    ];
+
+    for (i = 0; i < questions.length; i++) {
+        answers.push(readline.question(questions[i]));
+    }
+
         stream.write('<div>' + answers[0] + '</div> \n <div>' + answers[1] + '</div> \n <div>');
         stream.write('ID: ' + answers[2] + '</div> \n <div>')
         stream.write('Email: ' + answers[3] + '</div> \n <div>');
-        stream.write('GitHub: ' + answers[4] + '</div> \n <div>');
+        stream.write('GitHub: ' + answers[4] + '</div> \n <div> \n');
+
+}
+
+getInfo();
+getInfo();
+getInfo();
+
         stream.write('</body> \n </html>');
         stream.end();
     });
 
-    // // function to initialize program
-    function init() {
-    }
+function init() {
+}
 
-    // // function call to initialize program
-    init();
+init();
