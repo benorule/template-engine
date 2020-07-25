fc8727c614a1c678e1085773bf419c1a34c986b8
Benjamin Turnbull <benorule@outlook.com>
2020-07-25 18:54:09 +0930

    tried different solutions to problem of making function run i amount of time based on user input

--- server.js
+++ server.js
@@ -3,45 +3,44 @@ var readline = require('readline-sync');
 var inquirer = require('inquirer');
 
 var stream = fs.createWriteStream("index.html");
-    stream.once('open', function (fd) {
-        stream.write('<!DOCTYPE html> \n <html lang="en"> \n <head> \n <meta charset="UTF-8"> \n <meta name="viewport" content="width=device-width, initial-scale=1.0"> \n <title>Document</title> \n </head> \n <body>');
+stream.once('open', function (fd) {
+    stream.write('<!DOCTYPE html> \n <html lang="en"> \n <head> \n <meta charset="UTF-8"> \n <meta name="viewport" content="width=device-width, initial-scale=1.0"> \n <title>Document</title> \n </head> \n <body>');
 
-// const employees = parseInt(readline.question("Enter number of employees: "));
-// console.log(employees);
+    const employees = parseInt(readline.question("Enter number of employees: "));
+    console.log(employees);
 
-function getInfo() {
+    function getInfo() {
 
-    console.log("test");
+        console.log("test");
 
-    const questions = [
-        "Enter your name: ",
-        "Enter your role: ",
-        "Enter your ID number: ",
-        "Enter your Email address: ",
-        "Enter your GitHub username: "
-    ];
+        const questions = [
+            "Enter your name: ",
+            "Enter your role: ",
+            "Enter your ID number: ",
+            "Enter your Email address: ",
+            "Enter your GitHub username: "
+        ];
 
-    let answers = [
-    ];
+        let answers = [
+        ];
 
-    for (i = 0; i < questions.length; i++) {
-        answers.push(readline.question(questions[i]));
-    }
+        for (i = 0; i < questions.length; i++) {
+            answers.push(readline.question(questions[i]));
+        }
 
         stream.write('<div>' + answers[0] + '</div> \n <div>' + answers[1] + '</div> \n <div>');
         stream.write('ID: ' + answers[2] + '</div> \n <div>')
         stream.write('Email: ' + answers[3] + '</div> \n <div>');
         stream.write('GitHub: ' + answers[4] + '</div> \n <div> \n');
 
-}
+        employess = (employees - 1);
+    }
 
-getInfo();
-getInfo();
-getInfo();
+    getInfo();
 
-        stream.write('</body> \n </html>');
-        stream.end();
-    });
+    stream.write('</body> \n </html>');
+    stream.end();
+});
 
 function init() {
 }
